ac81ce302393b13f42510ad87b0d23667317d30f
Aaron Warshay <aaron.warshay@gmail.com>
2017-03-04 11:22:41 -0500

    missed a variable rename

--- src/DrawHeart.java
+++ src/DrawHeart.java
@@ -7,7 +7,7 @@ public class DrawHeart extends GraphicsProgram{
 	private GPoint m_leftPoint = new GPoint();
 	private GPoint m_rightPoint = new GPoint();
 	private GPoint m_bottomPoint = new GPoint();
-	private double m_unit;
+	private double m_units;
 	private double m_centerXPosition;
 	private double m_centerOfSquareY;
 	private double m_side;
@@ -24,17 +24,18 @@ public class DrawHeart extends GraphicsProgram{
 	{
 		m_unit = getHeight()/13;
 		m_centerXPosition = getWidth()/2;
-		m_centerOfSquareY = 7*m_unit;
-		m_upperPoint.setLocation( m_centerXPosition, 3*m_unit );
-		m_bottomPoint.setLocation( m_centerXPosition, 11*m_oneUnit );
-		m_rightPoint.setLocation( m_middlePoint + 4*m_unit, m_centerOfSquareY );
-		m_leftPoint.setLocation( m_middlePoint - 4*m_unit, m_centerOfSquareY );
+		m_centerOfSquareY = 7*m_units;
+		m_upperPoint.setLocation( m_centerXPosition, 3*m_units );
+		m_bottomPoint.setLocation( m_centerXPosition, 11*m_units );
+		m_rightPoint.setLocation( m_middlePoint + 4*m_units, m_centerOfSquareY );
+		m_leftPoint.setLocation( m_middlePoint - 4*m_units, m_centerOfSquareY );
 		GLine lineLeft = new GLine(m_leftPoint.getX(), m_leftPoint.getY(),
 					m_bottomPoint.getX(), m_bottomPoint.getY());
 		add(lineLeft);
 		GLine lineRight = new GLine(m_rightPoint.getX(), m_rightPoint.getY(),
 					m_bottomPoint.getX(), m_bottomPoint.getY());
 		add(lineRight);
+		m_side = Math.sin(8*m_units);
 		GArc leftHump = new GArc(m_middlePoint - 2.5*m_oneUnit, 0, 2.5*m_oneUnit, 2.5*m_oneUnit, 
 									45, 180);
 		add(leftHump);	
