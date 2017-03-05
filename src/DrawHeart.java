ecd568c1f85e6faba3b2bb37184639a39c1dfa9b
Aaron Warshay <aaron.warshay@gmail.com>
2017-03-04 23:30:20 -0500

    added some extra lines to orient, also had cosine upsidedown

--- src/DrawHeart.java
+++ src/DrawHeart.java
@@ -27,23 +27,32 @@ public class DrawHeart extends GraphicsProgram{
 		m_units = getHeight()/13; // one unit is 1/13 of the height, seemed to fit nicely
 		m_centerXPosition = getWidth()/2;
 		m_centerOfSquareY = 7*m_units;
-		m_radius = 4*m_units;
+		m_diagonal = 8*m_units;
 		m_upperPoint.setLocation( m_centerXPosition, 3*m_units );
 		m_bottomPoint.setLocation( m_centerXPosition, 11*m_units );
-		m_rightPoint.setLocation( m_centerXPosition + m_radius, m_centerOfSquareY );
-		m_leftPoint.setLocation( m_centerXPosition - m_radius, m_centerOfSquareY );
-		GLine lineLeft = new GLine(m_leftPoint.getX(), m_leftPoint.getY(),
+		m_rightPoint.setLocation( m_centerXPosition + m_diagonal/2, m_centerOfSquareY );
+		m_leftPoint.setLocation( m_centerXPosition - m_diagonal/2, m_centerOfSquareY );
+		GLine lineLeft = new GLine( m_leftPoint.getX(), m_leftPoint.getY(),
 					m_bottomPoint.getX(), m_bottomPoint.getY());
 		add(lineLeft);
-		GLine lineRight = new GLine(m_rightPoint.getX(), m_rightPoint.getY(),
+		GLine lineRight = new GLine( m_rightPoint.getX(), m_rightPoint.getY(),
 					m_bottomPoint.getX(), m_bottomPoint.getY());
 		add(lineRight);
-		m_side = Math.cos(45)*8*m_units;
-		m_diagonal = 8*m_units;
+		m_side = m_diagonal/2 / Math.cos(45);
+		GLine testLineLeft = new GLine( m_upperPoint.getX(), m_upperPoint.getY(),
+					m_leftPoint.getX(), m_leftPoint.getY());
+		add(testLineLeft);
+		GLine testLineRight = new GLine( m_upperPoint.getX(), m_upperPoint.getY(),
+					m_rightPoint.getX(), m_rightPoint.getY());
+		add(testLineRight); 
 		m_circleDiameter = m_side;
-		GArc leftHump = new GArc(m_centerXPosition + m_side - m_diagonal, 
-					m_centerOfSquareY-m_circleDiameter,
-					m_circleDiameter, m_circleDiameter, 45, 180);
+		GArc leftHump = new GArc( m_centerXPosition, 
+					m_centerOfSquareY,
+					m_side, m_side, 45, 180);
 		add(leftHump);
+		GArc rightHump = new GArc( m_centerXPosition,
+					m_centerOfSquareY,
+					m_side, m_side, -45, 180);
+		add(rightHump);
 	}
 }
