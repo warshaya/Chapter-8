8a4978441a1f38b3c7ed747a7b9005008e6cfd52
Aaron Warshay <aaron.warshay@gmail.com>
2017-03-07 13:00:04 -0500

    mocking up more of how I want run to work, needs 'Words' and 'Initials'

--- src/Initials.java
+++ src/Initials.java
@@ -3,13 +3,33 @@ import acm.graphics.*;
 
 public class Initials extends GraphicsProgram
 {
-
 	public void run()
 	{
-		firstInitial.drawStroke( STRAIGHT, LOWERLEFT, TOPMIDDLE );
-		firstInitial.drawStroke( STRAIGHT, TOPMIDDLE, LOWERRIGHT );
-		firstInitial.drawMiddleBar(); 
-	}
+		Initial firstInitial = new Initial();		
 
+		firstInitial.drawStroke( STRAIGHT, BOTTOMLEFT, TOPMIDDLE );
+		firstInitial.drawStroke( STRAIGHT, TOPMIDDLE, BOTTOMRIGHT );
+		firstInitial.drawMiddleBar( CENTER ); 
+
+		Initial secondInitial = new Initial();
+
+		secondInitial.drawStroke( STRAIGHT, TOPLEFT, TOPRIGHT );
+		secondInitial.drawStroke( STRAIGHT, BOTTOMLEFT, BOTTOMRIGHT );
+		secondInitial.drawStroke( STRAIGHT, TOPMIDDLE, BOTTOMMIDDLE );
+
+		Initial thirdInitial = new Initial();
+
+		thirdInitial.drawStroke( STRAIGHT, TOPLEFT, BOTTOMLEFT );
+		thirdInitial.drawStroke( STRAIGHT, BOTTOMLEFT, CENTER );
+		thirdInitial.drawStroke( STRAIGHT, CENTER, BOTTOMRIGHT );
+		thridInitial.drawStroke( STRAIGHT, BOTTOMRIGHT, TOPRIGHT );
+
+		Word word = new Word();
 		
+		word.add( firstInitial );
+		word.add( secondInitial );
+		word.add( thirdInitial );
+
+		add( word , getWidth()/2, getHeight()/2 );
+	}	
 }
