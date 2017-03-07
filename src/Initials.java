39eca8d7a974d7b0e0b0668ec889391f3290055c
Aaron Warshay <aaron.warshay@gmail.com>
2017-03-07 12:49:10 -0500

    defining how I want run to work, how clients might use it, xInitial.DrawStroke idea

--- src/Initials.java
+++ src/Initials.java
@@ -6,7 +6,10 @@ public class Initials extends GraphicsProgram
 
 	public void run()
 	{
-		
+		firstInitial.drawStroke( STRAIGHT, LOWERLEFT, TOPMIDDLE );
+		firstInitial.drawStroke( STRAIGHT, TOPMIDDLE, LOWERRIGHT );
+		firstInitial.drawMiddleBar(); 
 	}
-	
+
+		
 }
