b319b073b6a9436659b857234637e207bdb818eb
Aaron Warshay <aaron.warshay@gmail.com>
2017-03-09 13:02:37 -0500

    adding Position ENUM

--- src/Initials.java
+++ src/Initials.java
@@ -7,28 +7,35 @@ public enum Stroke
 	CURVY
 }
 
+public enum Position
+{
+	TOPLEFT, TOPMIDDLE, TOPRIGHT,
+	MIDDLELEFT, CENTER, MIDDLERIGHT,
+	BOTTOMLEFT, BOTTOMMIDDLE, BOTTOMRIGHT
+}
+
 public class Initials extends GraphicsProgram
 {
 	public void run()
 	{
 		Initial firstInitial = new Initial();		
 
-		firstInitial.drawStroke( Stroke.STRAIGHT, BOTTOMLEFT, TOPMIDDLE );
-		firstInitial.drawStroke( Stroke.STRAIGHT, TOPMIDDLE, BOTTOMRIGHT );
-		firstInitial.drawMiddleBar( CENTER ); 
+		firstInitial.drawStroke( Stroke.STRAIGHT, Position.BOTTOMLEFT, Position.TOPMIDDLE );
+		firstInitial.drawStroke( Stroke.STRAIGHT, Position.TOPMIDDLE, Position.BOTTOMRIGHT );
+		firstInitial.drawMiddleBar( Position.CENTER ); 
 
 		Initial secondInitial = new Initial();
 
-		secondInitial.drawStroke( Stroke.STRAIGHT, TOPLEFT, TOPRIGHT );
-		secondInitial.drawStroke( Stroke.STRAIGHT, BOTTOMLEFT, BOTTOMRIGHT );
-		secondInitial.drawStroke( Stroke.STRAIGHT, TOPMIDDLE, BOTTOMMIDDLE );
+		secondInitial.drawStroke( Stroke.STRAIGHT, Position.TOPLEFT, Position.TOPRIGHT );
+		secondInitial.drawStroke( Stroke.STRAIGHT, Position.BOTTOMLEFT, Position.BOTTOMRIGHT );
+		secondInitial.drawStroke( Stroke.STRAIGHT, Position.TOPMIDDLE, Position.BOTTOMMIDDLE );
 
 		Initial thirdInitial = new Initial();
 
-		thirdInitial.drawStroke( Stroke.STRAIGHT, TOPLEFT, BOTTOMLEFT );
-		thirdInitial.drawStroke( Stroke.STRAIGHT, BOTTOMLEFT, CENTER );
-		thirdInitial.drawStroke( Stroke.STRAIGHT, CENTER, BOTTOMRIGHT );
-		thridInitial.drawStroke( Stroke.STRAIGHT, BOTTOMRIGHT, TOPRIGHT );
+		thirdInitial.drawStroke( Stroke.STRAIGHT, Position.TOPLEFT, Position.BOTTOMLEFT );
+		thirdInitial.drawStroke( Stroke.STRAIGHT, Position.BOTTOMLEFT, Position.CENTER );
+		thirdInitial.drawStroke( Stroke.STRAIGHT, Position.CENTER, Position.BOTTOMRIGHT );
+		thridInitial.drawStroke( Stroke.STRAIGHT, Position.BOTTOMRIGHT, Position.TOPRIGHT );
 
 		Word word = new Word();
 		
