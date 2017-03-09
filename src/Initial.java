ad9ff83c03d77f132f7102d4c637f35c4f8b95e6
Aaron Warshay <aaron.warshay@gmail.com>
2017-03-09 12:58:16 -0500

    adding StrokeType ENUM for straight vs Curvy

---
+++ src/Initial.java
@@ -0,0 +1,13 @@
+import acm.graphics.*;
+
+public class Initial extends GCompound {
+
+	public void drawStroke( strokeStyle style, strokePosition start, strokePosition end )
+	{
+		if ( style == STRAIGHT )
+		{
+			
+		}
+	} 
+
+}
--- src/Initials.java
+++ src/Initials.java
@@ -1,28 +1,34 @@
 import acm.program.*;
 import acm.graphics.*;
 
+public enum Stroke
+{
+	STRAIGHT,
+	CURVY
+}
+
 public class Initials extends GraphicsProgram
 {
 	public void run()
 	{
 		Initial firstInitial = new Initial();		
 
-		firstInitial.drawStroke( STRAIGHT, BOTTOMLEFT, TOPMIDDLE );
-		firstInitial.drawStroke( STRAIGHT, TOPMIDDLE, BOTTOMRIGHT );
+		firstInitial.drawStroke( Stroke.STRAIGHT, BOTTOMLEFT, TOPMIDDLE );
+		firstInitial.drawStroke( Stroke.STRAIGHT, TOPMIDDLE, BOTTOMRIGHT );
 		firstInitial.drawMiddleBar( CENTER ); 
 
 		Initial secondInitial = new Initial();
 
-		secondInitial.drawStroke( STRAIGHT, TOPLEFT, TOPRIGHT );
-		secondInitial.drawStroke( STRAIGHT, BOTTOMLEFT, BOTTOMRIGHT );
-		secondInitial.drawStroke( STRAIGHT, TOPMIDDLE, BOTTOMMIDDLE );
+		secondInitial.drawStroke( Stroke.STRAIGHT, TOPLEFT, TOPRIGHT );
+		secondInitial.drawStroke( Stroke.STRAIGHT, BOTTOMLEFT, BOTTOMRIGHT );
+		secondInitial.drawStroke( Stroke.STRAIGHT, TOPMIDDLE, BOTTOMMIDDLE );
 
 		Initial thirdInitial = new Initial();
 
-		thirdInitial.drawStroke( STRAIGHT, TOPLEFT, BOTTOMLEFT );
-		thirdInitial.drawStroke( STRAIGHT, BOTTOMLEFT, CENTER );
-		thirdInitial.drawStroke( STRAIGHT, CENTER, BOTTOMRIGHT );
-		thridInitial.drawStroke( STRAIGHT, BOTTOMRIGHT, TOPRIGHT );
+		thirdInitial.drawStroke( Stroke.STRAIGHT, TOPLEFT, BOTTOMLEFT );
+		thirdInitial.drawStroke( Stroke.STRAIGHT, BOTTOMLEFT, CENTER );
+		thirdInitial.drawStroke( Stroke.STRAIGHT, CENTER, BOTTOMRIGHT );
+		thridInitial.drawStroke( Stroke.STRAIGHT, BOTTOMRIGHT, TOPRIGHT );
 
 		Word word = new Word();
 		
