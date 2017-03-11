cedc95a45772c97d5d4f34a92aa303af32e14f36
Aaron Warshay <aaron.warshay@gmail.com>
2017-03-11 13:11:39 -0500

    adding consts for straight and curvy

--- src/Initial.java
+++ src/Initial.java
@@ -2,9 +2,27 @@ import acm.graphics.*;
 
 public class Initial extends GCompound {
 
-	public void drawStroke( Style style, Position start, Position end )
+	public final static int STYLE = 0;
+	public final static int CURVY = 1;
+
+	public final static GPoint TOPLEFT = new GPoint( -10, -20 );
+	public final static GPoint TOPMIDDLE = new GPoint( 0, -20 );
+	public final static GPoint TOPRIGHT = new GPoint( 10, -20 );
+	public final static GPoint MIDDLELEFT = new GPoint( -10, 0 );
+	public final static GPoint CENTER = new GPoint();
+	public final static GPoint MIDDLERIGHT = new GPoint( 10, 0 );
+	public final static GPoint BOTTOMLEFT = new GPoint( -10, 20 );
+	public final static GPoint BOTTOMMIDDLE = new GPoint( 0, 20 );
+	public final static GPoint BOTTOMRIGHT = new GPoint( 10, 20 );
+
+	public void drawStroke( int style, GPoint start, GPoing end )
 	{
-		if ( style == Style.STRAIGHT )
+		if ( style == CURVY )
+		{
+
+		}
+
+		else if ( style == STRAIGHT )
 		{
 			GLine line = new GLine();
 			line.setStartPoint( start.getX(), start.getY() );
