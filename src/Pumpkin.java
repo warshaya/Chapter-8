59d079b313d8f8c732f7ec6a3f9596d68e6e1ce3
Aaron Warshay <aaron.warshay@gmail.com>
2017-03-19 15:11:11 -0400

    using constants to specifiy eye position and scale

--- src/Pumpkin.java
+++ src/Pumpkin.java
@@ -4,16 +4,17 @@ public class Pumpkin extends GCompound
 {	
 	private GObject m_nose = new GRect( 10, 10 );
 	private GObject m_stem = new GRect( 5, 5 );
-	private GObject m_eyes = new PumpkinEyes();
+	private GCompound m_eyes = new PumpkinEyes();
 	private GObject m_mouth = new GRect( 30, 30);
 	private GObject m_body = new PumpkinBody( PumpkinDrawer.BODYWIDTH, PumpkinDrawer.BODYHEIGHT );
 	
 	public Pumpkin()
 	{
-	add(m_body);
-	add(m_stem);
-	add(m_eyes, 0, -5 );
-	add(m_mouth);
-	add(m_nose);
+	add( m_body );
+	add( m_stem );
+	m_eyes.scale( 10, 10 );
+	add( m_eyes, 0, PumpkinDrawer.EYE_LEVEL );
+	add( m_mouth );
+	add( m_nose );
 	}
 }
