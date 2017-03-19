7c01bd5a2e6ac8e793586ec3f79a6419957753e5
Aaron Warshay <aaron.warshay@gmail.com>
2017-03-18 22:39:27 -0400

    filled out variables for the Pumpkin class

--- src/Pumpkin.java
+++ src/Pumpkin.java
@@ -1,6 +1,22 @@
 import acm.graphics.*;
 
 public class Pumpkin extends GCompound
-{
+{	
+	private Nose m_nose;
+	private Stem m_stem;
+	private Eyes m_eyes;
+	private Mouth m_mouth;
+	private Body m_body;
+	
+	public Pumpkin()
+	{
+
+	}
+
+	add(m_body);
+	add(m_stem);
+	add(m_eyes);
+	add(m_mouth);
+	add(m_nose);
 	
 }
