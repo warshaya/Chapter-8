e5b9d2e29be6ffb706c0ba14f3f57ba0303b5896
warshaya <aaron.warshay@gmail.com>
2017-05-08 20:50:28 -0400

    use m_door (to be created) in GHouse

--- src/GHouse.java
+++ src/GHouse.java
@@ -6,12 +6,17 @@ public class GHouse extends GCompound
 	private GObject m_roof;
 	private GObject m_door;
 
-	public GHouse( double wallWidth, double wallHeight , double roofHeight)
+	public GHouse( double wallWidth, double wallHeight, 
+			double roofHeight, 
+			double doorWidth, double doorHeight, double knobSize)
 	{
 		m_walls = new GRect( wallWidth, wallHeight);
 		add( m_walls, 0, roofHeight);
 
 		m_roof = new GRoof( wallWidth, roofHeight );
 		add ( m_roof, 0, 0 );
+
+		m_door = new GDoor( doorWidth, doorHeight, knobSize );
+		add ( m_door, this.getWidth()/2, this.getHeight()-m_door.getHeight() );
 	}
 }
