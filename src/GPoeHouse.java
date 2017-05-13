5e578ba1448b3efc3929676b851842b3a998c8ce
warshaya <aaron.warshay@gmail.com>
2017-05-13 12:06:28 -0400

    creating GTower to use, used GWindow, added constants to specify the dimensions

--- src/GPoeHouse.java
+++ src/GPoeHouse.java
@@ -2,30 +2,48 @@ import acm.graphics.*;
 
 public class GPoeHouse extends GCompound
 {
-	private GObject m_house;
-	private GObject m_turretOne;
-	private GObject m_turretTwo;
-	private GObject m_door;
+	public GPoeHouse( double houseBaseWidth, double houseBaseHeight, double hallRoofHeight,
+				double leftTurretBaseWidth, double leftTurretBaseHeight, 
+				double leftTurretRoofHeight,
+				double rightTurretBaseWidth, double rightTurretBaseHeight, 
+				double rightTurretRoofHeight,
+				double doorBaseWidth, double doorBaseHeight, double doorRoofHeight,
+				double leftWindowWidth, double leftWindowHeight,
+				double rightWindowWidth, double rightWindowHeight, 
+				double leftWindowDistanceFromWall, double leftWindowDistanceFromGround,
+				double rightWindowDistanceFromWall, double rightWindowDistanceFromGround )
+	{
+		m_house = new GTower( houseBaseWidth, houseBaseHeight, hallRoofHeight);
+		m_leftTurret = new GTower( leftTurretBaseWidth, leftTurretBaseHeight, leftTurretRoofHeight );
+		m_rightTurret = new GTower( rightTurretBaseWidth, rightTurretBaseHeight, rightTurretRoofHeight );
+		m_door = new GTower( doorBaseWidth, doorBaseHeight, doorRoofHeight );
+		m_leftWindow = new GWindow( leftWindowWidth, leftWindowHeight, 1, 1 );
+		m_rightWindow = new GWindow( rightWindowWidth, rightWindowHeight, 1, 1 );
+	
+		m_height = Math.max( Math.max( m_house.getHeight(), m_leftTurret.getHeight() ),
+					m_rightTurret.getHeight() );
 
+		m_width = Math.max( Math.max( m_house.getWidth(), m_leftTurret.getWidth() ),
+					m_rightTurret.getWidth() );
+
+		add( m_house, m_leftTurret.getWidth(), m_height-m_house.getHeight() );
+		add( m_leftTurret, 0, m_height-m_leftTurret.getHeight() );
+		add( m_rightTurret, m_leftTurret.getWidth() + m_house.getWidth(),
+			m_height-m_rightTurret.getHeight() );
+		add( m_door, m_leftTurret.getWidth()+m_house.getWidth()/2-m_door.getWidth()/2,
+			m_height-m_door.getHeight() );
+		add( m_leftWindow, m_house.getX()+leftWindowDistanceFromWall,
+			m_height-leftWindowDistanceFromGround-m_leftWindow.getHeight() );
+		add( m_rightWindow, m_rightTurret.getX()-m_rightWindow.getWidth()-rightWindowDistanceFromWall,
+			m_height-rightWindowDistanceFromGround-m_rightWindow.getHeight() );
+	}
+
+	private GObject m_house;
+	private GObject m_leftTurret;
+	private GObject m_rightTurret;
+	private GObject m_door;
 	private GObject m_leftWindow;
 	private GObject m_rightWindow;	
-
 	private double m_height;
 	private double m_width;
-
-	public GPoeHouse()
-	{
-		m_height = Math.max( Math.max( m_house.getHeight(), m_turretOne.getHeight() ),
-					m_turretTwo.getHeight() );
-
-		m_width = Math.max( Math.max( m_house.getWidth(), m_turretOne.getWidth() ),
-					m_turretTwo.getWidth() );
-
-		add( m_house, m_turretOne.getWidth(), m_height-m_house.getHeight() );
-		add( m_turretOne, 0, m_height );
-		add( m_turretTwo, m_turretOne.getWidth() + m_house.getWidth(), m_height );
-		add( m_door, m_turretOne.getWidth()+m_house.getWidth()/2-m_door.getWidth()/2,
-				m_height-m_house.getHeight()-m_door.getHeight() );
-		
-	}
 }
--- src/HouseOfUsher.java
+++ src/HouseOfUsher.java
@@ -9,12 +9,42 @@ import acm.graphics.*;
 
 public class HouseOfUsher extends GraphicsProgram
 {
-	private GObject m_houseOfUsher;	
-	
+	public static final double HOUSE_BASE_WIDTH = 350;
+	public static final double HOUSE_BASE_HEIGHT = 200;
+	public static final double HOUSE_ROOF_HEIGHT = 100;
+	public static final double LEFT_TURRET_BASE_WIDTH = 100;
+	public static final double LEFT_TURRET_BASE_HEIGHT = 250;
+	public static final double LEFT_TURRET_ROOF_HEIGHT = 200;
+	public static final double RIGHT_TURRET_BASE_WIDTH = 50;
+	public static final double RIGHT_TURRET_BASE_HEIGHT = 270;
+	public static final double RIGHT_TURRET_ROOF_HEIGHT = 80;
+	public static final double DOOR_BASE_WIDTH = 30;
+	public static final double DOOR_BASE_HEIGHT = 50;
+	public static final double DOOR_ROOF_HEIGHT = 20;
+	public static final double LEFT_WINDOW_WIDTH = 60;
+	public static final double LEFT_WINDOW_HEIGHT = 40;
+	public static final double RIGHT_WINDOW_WIDTH = 80;
+	public static final double RIGHT_WINDOW_HEIGHT = 60;
+	public static final double LEFT_WINDOW_DISTANCE_FROM_WALL = 4;
+	public static final double LEFT_WINDOW_DISTANCE_FROM_GROUND = 100;
+	public static final double RIGHT_WINDOW_DISTANCE_FROM_WALL = 8;
+	public static final double RIGHT_WINDOW_DISTANCE_FROM_GROUND = 124;
+
 	public void run()
 	{
-		m_houseOfUsher = new GPoeHouse();
+		m_houseOfUsher = new GPoeHouse( HOUSE_BASE_WIDTH, HOUSE_BASE_HEIGHT, HOUSE_ROOF_HEIGHT,
+						LEFT_TURRET_BASE_WIDTH, LEFT_TURRET_BASE_HEIGHT, 
+						LEFT_TURRET_ROOF_HEIGHT,
+						RIGHT_TURRET_BASE_WIDTH, RIGHT_TURRET_BASE_HEIGHT,
+						RIGHT_TURRET_ROOF_HEIGHT,
+						DOOR_BASE_WIDTH, DOOR_BASE_HEIGHT, DOOR_ROOF_HEIGHT,
+						LEFT_WINDOW_WIDTH, LEFT_WINDOW_HEIGHT,
+						RIGHT_WINDOW_WIDTH, RIGHT_WINDOW_HEIGHT,
+						LEFT_WINDOW_DISTANCE_FROM_WALL, LEFT_WINDOW_DISTANCE_FROM_GROUND,
+						RIGHT_WINDOW_DISTANCE_FROM_WALL, RIGHT_WINDOW_DISTANCE_FROM_GROUND );
 		add( m_houseOfUsher, getWidth()/2 - m_houseOfUsher.getWidth()/2,
 					getHeight() - m_houseOfUsher.getHeight() );	
 	}
+
+	private GObject m_houseOfUsher;	
 }
