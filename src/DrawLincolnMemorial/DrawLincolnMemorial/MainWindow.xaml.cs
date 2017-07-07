7aea63c1a1833fcb8f9dd26a21aa52a53fcd1c55
Aaron Warshay <a.warshay@techsmith.com>
2017-07-07 13:08:38 -0400

    initializing a WPF program with MainWindow and a canvas

---
+++ src/DrawLincolnMemorial/DrawLincolnMemorial/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DrawLincolnMemorial
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+         createAndAddLine();
+      }
+
+      private void createAndAddLine()
+      {
+         var myLine = new Line();
+         myLine.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
+         myLine.X1 = 1;
+         myLine.X2 = 50;
+         myLine.Y1 = 1;
+         myLine.Y2 = 25;
+         myLine.HorizontalAlignment = HorizontalAlignment.Left;
+         myLine.VerticalAlignment = VerticalAlignment.Center;
+         myLine.StrokeThickness = 4;
+         myCanvas.Children.Add( myLine );
+      }
+   }
+}
